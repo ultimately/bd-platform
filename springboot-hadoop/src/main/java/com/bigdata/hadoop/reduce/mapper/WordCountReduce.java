93ff8f5b505da095efc0319bb6d1be9bc02fe599
ultimately <1018633688@qq.com>
2021-12-22 23:54:59 +0800

    单词统计

--- springboot-hadoop/src/main/java/com/bigdata/hadoop/reduce/mapper/WordCountReduce.java
+++ springboot-hadoop/src/main/java/com/bigdata/hadoop/reduce/mapper/WordCountReduce.java
@@ -1,5 +1,6 @@
 package com.bigdata.hadoop.reduce.mapper;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -14,17 +15,17 @@ import java.util.List;
  * @Author levlin
  * @Date 2021/12/20
  **/
+@Slf4j
 public class WordCountReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
 
     private IntWritable result = new IntWritable();
-    private String text = "孙权";
+    private String text = "吕";
     private int textSum = 0;
     private List<String> textList = null;
 
     public WordCountReduce() {
         textList = new ArrayList<>();
-        textList.add("曹操");
-        textList.add("孙权");
+        textList.add(text);
     }
 
     @Override
@@ -39,16 +40,15 @@ public class WordCountReduce extends Reducer<Text, IntWritable, Text, IntWritabl
 
         String keyStr = key.toString();
         // 未使用分词器，需要根据map传过来的行内容检索并累加
-        // boolean isHas = keyStr.contains(text);
-        // if (isHas) {
-        // textSum++;
-        // System.out.println("============ " + text + " 统计分词为: " + textSum + "
-        // ============");
-        // }
+         boolean isHas = keyStr.contains(text);
+         if (isHas) {
+         textSum++;
+         log.info("统计文字：{}，统计次数：{}", text, textSum);
+         }
 
         // 使用分词器，内容已经被统计好了，直接输出即可
         if (textList.contains(keyStr)) {
-            System.out.println("============ " + keyStr + " 统计分词为: " + sum + " ============");
+            log.info("统计文字分词：{}，统计分词次数：{}", keyStr, sum);
         }
     }
 }
